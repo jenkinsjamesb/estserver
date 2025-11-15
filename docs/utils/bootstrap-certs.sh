c6cd818959c698d86a38db92d4358d7fe078b86d
James Jenkins <jenkins.j.brad@gmail.com>
2025-11-14 19:44:31 -0500

    Remove files

--- docs/utils/bootstrap-certs.sh
+++
@@ -1,8 +0,0 @@
-# dest dir, subj line
-sh generate-root-ca.sh . "/C=US/ST=Virginia/L=Richmond/O=--/OU=--/CN=example/"
-
-# dest dir, root ca dir, subj line
-sh create-intermediate.sh . ./root-ca "/C=US/ST=Virginia/L=Richmond/O=--/OU=--/CN=example/"
-
-# server name, CA dir, subj line, ext line
-sh create-server.sh estserver ./intermediate-ca "/C=US/ST=Virginia/L=Richmond/O=--/OU=--/CN=example/", "subjectAltName = DNS:example.localdomain" 
